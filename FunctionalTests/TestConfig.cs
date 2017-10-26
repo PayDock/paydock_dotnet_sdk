c5cef990fec6cf584a9c0b43b1c40f3333a4cb20
dcamdupe <david@uberconcept.com>
2017-10-26 15:38:38 +1100

    New release for stripe connect

--- FunctionalTests/TestConfig.cs
+++ FunctionalTests/TestConfig.cs
@@ -4,12 +4,12 @@ namespace FunctionalTests
 {
     public static class TestConfig
     {
-        public const string SecretKey = "";
-        public const string GatewayId = "";
-        public const string PaypalGatewayId = "";
-        public const string PublicKey = "";
-        public const string OverideSecretKey = "";
-        public const string OveridePublicKey = "";
+        public const string SecretKey = "fccbf57c8a65a609ed86edd417177905bfd5a99b";
+        public const string GatewayId = "58377235377aea03343240cc";
+        public const string PaypalGatewayId = "58bf55343c541b5b87f741bd";
+        public const string PublicKey = "cc5bedb53a1b64491b5b468a2486b32cc250cda2";
+        public const string OverideSecretKey = "fccbf57c8a65a609ed86edd417177905bfd5a99b";
+        public const string OveridePublicKey = "cc5bedb53a1b64491b5b468a2486b32cc250cda2";
 
         public static void Init()
         {
