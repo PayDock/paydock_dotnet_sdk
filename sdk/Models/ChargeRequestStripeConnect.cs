07e3eee6609fd6e6f404b120fe68f286751a36f0
dcamdupe <david@uberconcept.com>
2017-10-26 11:53:42 +1100

    Update stripe connect objects based on changes to the API

--- sdk/Models/ChargeRequestStripeConnect.cs
+++ sdk/Models/ChargeRequestStripeConnect.cs
@@ -3,20 +3,27 @@
     public class ChargeRequestStripeConnect : ChargeRequestBase
 	{
 		public MetaData meta;
+		public Transfer[] transfer;
 	}
 
 	public class MetaData
 	{
 		public string stripe_direct_account_id;
+		public decimal stripe_application_fee;
 		public string stripe_destination_account_id;
-		public string stripe_transfer_group;
-		public Transfer[] stripe_transfer;
+		public decimal stripe_destination_amount;
 	}
 
 	public class Transfer
 	{
-		public decimal amount;
-		public string currency;
-		public string destination;
+		public string stripe_transfer_group;
+		public TransferItems[] items;
+
+		public class TransferItems
+		{
+			public decimal amount;
+			public string currency;
+			public string destination;
+		}
 	}
 }
