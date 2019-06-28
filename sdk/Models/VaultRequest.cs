bb2a92eba35287e0438a14163dbbcd57a3c4ebf1
Samuel Butler <samuel@koben.com.au>
2019-06-28 10:05:10 +1000

    Added missing token property

--- sdk/Models/VaultRequest.cs
+++ sdk/Models/VaultRequest.cs
@@ -2,6 +2,7 @@
 {
 	public class VaultRequest
 	{
+		public string token { get; set; }
 		public string card_name { get; set; }
 		public string card_number { get; set; }
 		public string expire_year { get; set; }
