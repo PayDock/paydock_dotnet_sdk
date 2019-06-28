71941defcdfa403695ff66642c4f2bbbc8361251
Samuel Butler <samuel@koben.com.au>
2019-06-28 10:11:09 +1000

    Added missing account_name, account_number, account_bsb properties to VaultRequest

--- sdk/Models/VaultRequest.cs
+++ sdk/Models/VaultRequest.cs
@@ -7,5 +7,8 @@
 		public string card_number { get; set; }
 		public string expire_year { get; set; }
 		public string expire_month { get; set; }
+		public string account_name { get; set; }
+		public string account_number { get; set; }
+		public string account_bsb { get; set; }
 	}
 }
