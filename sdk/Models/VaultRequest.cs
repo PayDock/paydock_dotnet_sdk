9cb2df70ace4cd2aef93736beb840d245d4bccae
Samuel Butler <samuel@koben.com.au>
2019-06-28 10:12:22 +1000

    Added missing type property to VaultRequest

--- sdk/Models/VaultRequest.cs
+++ sdk/Models/VaultRequest.cs
@@ -1,7 +1,12 @@
-﻿namespace Paydock_dotnet_sdk.Models
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+
+namespace Paydock_dotnet_sdk.Models
 {
 	public class VaultRequest
 	{
+		[JsonConverter(typeof(StringEnumConverter))]
+		public PaymentType type { get; set; }
 		public string token { get; set; }
 		public string card_name { get; set; }
 		public string card_number { get; set; }
