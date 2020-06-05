c0ab967e6877cb116897a5b8c75a08ccbc86d85c
Oleksandr Omelchenko <alex@paydock.com>
2020-06-05 20:43:33 +0300

    added properties to Transaction object in ChargeResponse and ChargeRefundResponce obj

--- sdk/Models/ChargeRefundResponse.cs
+++ sdk/Models/ChargeRefundResponse.cs
@@ -62,6 +62,11 @@ namespace Paydock_dotnet_sdk.Models
             public decimal amount { get; set; }
             public DateTime created_at { get; set; }
             public string currency { get; set; }
+            public string error_code { get; set; }
+            public string error_message { get; set; }
+            public string gateway_specific_code { get; set; }
+            public string gateway_specific_description { get; set; }
+            public string _source_ip_address { get; set; }
             public string status { get; set; }
             public string type { get; set; }
             public DateTime pended_at { get; set; }
