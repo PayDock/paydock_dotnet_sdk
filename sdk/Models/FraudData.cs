7e610885bf6bf3379c25dccc52a25bb41d49bd31
Oleksandr Omelchenko <alex@paydock.com>
2020-07-17 15:53:19 +0300

    service Id in fraud

--- sdk/Models/FraudData.cs
+++ sdk/Models/FraudData.cs
@@ -4,6 +4,7 @@ namespace Paydock_dotnet_sdk.Models
 {
     public class FraudData
     {
+        public string service_id { get; set; }
         public string token { get; set; }
         public dynamic data { get; set; }
     }
