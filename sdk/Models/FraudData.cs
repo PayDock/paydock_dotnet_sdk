3b341c2ab9ee40feb3ea625e6f33f6633c62c2ce
Oleksandr Omelchenko <alex@paydock.com>
2020-07-21 11:00:54 +0300

    FraudData  mode field

--- sdk/Models/FraudData.cs
+++ sdk/Models/FraudData.cs
@@ -5,6 +5,7 @@ namespace Paydock_dotnet_sdk.Models
     public class FraudData
     {
         public string service_id { get; set; }
+        public string mode { get; set; }
         public string token { get; set; }
         public dynamic data { get; set; }
     }
